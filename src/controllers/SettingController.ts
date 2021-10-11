8502acd8995c859ceccbfeb58b9c67b974773d23
Wagner Freitas <wagner.sousa.f@gmail.com>
2021-10-11 17:45:27 -0300

    first commit

---
+++ src/controllers/SettingController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+
+import { SettingRepository } from '../repositories/SettingRepository';
+
+class SettingController {
+  async create(request: Request, response: Response) {
+    const { chat, username } = request.body;
+
+    const settingsRepository = getCustomRepository(SettingRepository);
+
+    const settings = settingsRepository.create({
+      chat,
+      username,
+    });
+
+    await settingsRepository.save(settings);
+    return response.json(settings);
+  }
+}
+
+export { SettingController }
